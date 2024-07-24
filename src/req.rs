81bdfc48ebe4e4f5975cc173b5a82bd7c9a5e5f3
= <tomasz_nehring@outlook.com>
2024-07-24 11:47:53 +0200

    refactoring focused on ./src/req.rs

--- src/req.rs
+++ src/req.rs
@@ -4,6 +4,7 @@ pub use post_msg::PostMsg;
 pub mod post_msg;
 
 pub use get_msgs::GetMsgs;
+use serde::de::DeserializeOwned;
 pub mod get_msgs;
 
 use std::io::Read;
@@ -13,37 +14,21 @@ use anyhow::{anyhow, Context};
 pub fn get_msgs(
     client: &reqwest::blocking::Client,
     get_msgs_query: &crate::req::GetMsgs,
-) -> reqwest::Result<reqwest::blocking::Response> {
-    client
+) -> anyhow::Result<Vec<crate::model::ChatMsg>> {
+    let resp = client
         .get("http://localhost:8080/v1/msg")
         .query(get_msgs_query)
-        .send()
+        .send();
+    interpret_resp(resp)
 }
 pub fn get_msgs_with_ctx_err(
     client: &reqwest::blocking::Client,
     get_msgs_query: &crate::req::GetMsgs,
 ) -> anyhow::Result<Vec<crate::model::ChatMsg>> {
-    (|| -> anyhow::Result<_> {
-        let mut resp =
-            get_msgs(client, get_msgs_query).context("Failed to connect with the server.")?;
-        if resp.status().is_success() {
-            match resp.json::<Vec<crate::model::ChatMsg>>() {
-                Ok(messages) => Ok(messages),
-                Err(err) => Err(anyhow!("Failed to deserialize server's response: {err}")),
-            }
-        } else {
-            let mut resp_body = vec![0; 1024];
-            let read_count = resp.read(&mut resp_body).unwrap_or_else(|err| {
-                tracing::warn!("Request body-reading error was silenced: {err}");
-                0
-            });
-            resp_body.truncate(read_count);
-            let resp_body = String::from_utf8_lossy(&resp_body);
-            Err(anyhow!("Server returned:\n{resp:?}\n\n{}...", resp_body))
-        }
-    })()
-    .with_context(|| "Failed to get chat messages.")
+    get_msgs(client, get_msgs_query)
+        .with_context(|| "Failed to get chat messages.")
 }
+
 pub fn post_msg(
     client: &reqwest::blocking::Client,
     post_msg_query: &crate::req::PostMsg,
@@ -57,24 +42,41 @@ pub fn post_msg_with_ctx_err(
     client: &reqwest::blocking::Client,
     post_msg_query: &crate::req::PostMsg,
 ) -> anyhow::Result<crate::model::ChatMsg> {
-    (|| -> anyhow::Result<_> {
-        let mut resp =
-            post_msg(client, post_msg_query).context("Failed to connect with the server.")?;
-        if resp.status().is_success() {
-            match resp.json::<crate::model::ChatMsg>() {
-                Ok(message) => Ok(message),
-                Err(err) => Err(anyhow!("Failed to deserialize server's response: {err}")),
-            }
-        } else {
-            let mut resp_body = vec![0; 1024];
-            let read_count = resp.read(&mut resp_body).unwrap_or_else(|err| {
-                tracing::warn!("Request body-reading error was silenced: {err}");
-                0
-            });
-            resp_body.truncate(read_count);
-            let resp_body = String::from_utf8_lossy(&resp_body);
-            Err(anyhow!("Server returned:\n{resp:?}\n\n{}...", resp_body))
-        }
+    (|| -> anyhow::Result<crate::model::ChatMsg> {
+        let resp = post_msg(client, post_msg_query);
+        interpret_resp(resp)
     })()
-    .with_context(|| "Failed to post user-provided chat message.")
+    .with_context(|| {
+        "Failed to post user-provided chat message."
+    })
+}
+
+fn interpret_resp<D>(
+    resp: Result<reqwest::blocking::Response, reqwest::Error>,
+) -> Result<D, anyhow::Error>
+where
+    D: DeserializeOwned,
+{
+    let mut resp =
+        resp.context("Failed to connect with the server.")?;
+    if resp.status().is_success() {
+        match resp.json::<D>() {
+            Ok(obj) => Ok(obj),
+            Err(err) => Err(anyhow!(
+                "Failed to deserialize server's response: {err}"
+            )),
+        }
+    } else {
+        let mut resp_body = vec![0; 1024];
+        let read_count = resp.read(&mut resp_body).unwrap_or_else(|err| {
+            tracing::warn!("Error caught while reading request body was silenced: {err}");
+            0
+        });
+        resp_body.truncate(read_count);
+        let resp_body = String::from_utf8_lossy(&resp_body);
+        Err(anyhow!(
+            "Server returned:\n{resp:?}\n\n{}...",
+            resp_body
+        ))
+    }
 }
